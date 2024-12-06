2696d6501bbdcc8c22e898e5ef85b98f97907017
tankmaster48 <800018469@fjuhsd.org>
2024-12-06 14:28:14 -0800

    game

---
+++ StartLight.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartLight here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartLight extends Actor
+{
+    /**
+     * Act - do whatever the StartLight wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int i = 0;
+    public void act()
+    {
+        i++;
+        if (i % 50 == 0) nextSprite(i / 50);
+    }
+    
+    public void nextSprite(int i) {
+        switch (i) {
+            case 0:
+                break;
+            case 1:
+                setImage("yellow.png");
+                break;
+            case 2:
+                setImage("green1.png");
+                break;
+            case 3:
+                setImage("green2.png");
+                break;
+            case 4:
+                setImage("green3.png");
+                break;
+            case 5:
+                setImage("green4.png");
+                break;
+            case 6:
+                setImage("green5.png");
+                MyWorld world = (MyWorld) getWorld();
+                world.setFreeze(false);
+                world.startMusic();
+                world.removeObject(this);
+                break;
+        }
+    }
+}
